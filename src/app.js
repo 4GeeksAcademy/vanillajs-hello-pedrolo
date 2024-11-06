6614f451cdbc61de824d00143c98656062e873e9
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2024-11-06 20:11:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  pronoun.map(pro => {
+    adj.map(adje => {
+      noun.map(nombre => {
+        console.log(pro + adje + nombre + ".com");
+      });
+    });
+  });
 };
